be3dce5ead9e66ee637223cc09bed8bdc3c73037
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-03 15:50:29 +0100

    sequelize connection

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,31 @@
-import { sequelize } from "sequelize";
+import { Sequelize } from "sequelize";
 
 const { PGHOST, PGUSER, PGDATABASE, PGPASSWORD, PGPORT } = process.env;
 
-const sequelize = new sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
+const sequelize = new Sequelize(PGDATABASE, PGUSER, PGPASSWORD, {
   host: PGHOST,
   port: PGPORT,
   dialect: "postgres",
 });
+
 console.log("sequelize handshake created");
+
+export const testConnection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection established with database");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const connectDB = async () => {
+  try {
+    console.log("syncronizes all tables in connectDB");
+    await sequelize.sync();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default sequelize;
