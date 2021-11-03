563f3353389c70f42eb24a4b8e8ee4c41c01eb02
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-03 15:22:03 +0100

    project created

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import listEndpoints from "express-list-endpoints";
+import productsRoute from "./services/products/routes.js";
+import reviewsRoute from "./services/products/routes.js";
+import createDefaultTables from "./db/create-tables.js";
+
+const server = express();
+
+const { PORT } = process.env;
+
+server.use(express.json());
+
+server.use("/products", productsRoute);
+server.use("/reviews", reviewsRoute);
+
+console.table(listEndpoints(server));
+server.listen(PORT, async () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+  await createDefaultTables();
+});
+
+server.on("error", console.log);
