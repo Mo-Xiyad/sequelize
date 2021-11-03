89e8d0a1c6717f87bc3f64105bd76cbfd9594924
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-03 16:29:50 +0100

    tables

---
+++ src/db/models/index.js
@@ -0,0 +1,7 @@
+import Product from "./products.js";
+import Review from "./reviews.js";
+
+Product.hasMany(Review, { onDelete: "CASCADE" });
+Review.belongsTo(Product, { onDelete: "CASCADE" });
+
+export default { Product, Review };
