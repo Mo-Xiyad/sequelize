40a70686405766663c56689b44f3cf9c5684683c
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-03 16:00:02 +0100

    product model created

--- src/db/models/products.js
+++ src/db/models/products.js
@@ -1,1 +1,37 @@
 import sequelize from "../index.js";
+import s from "sequelize";
+const { DataTypes } = s;
+
+const Product = sequelize.define("product", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  brand: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  category: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+export default Product;
