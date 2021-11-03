a060f1993d35b9ff36b4e57e1b0b4c59ddb492cd
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-03 15:50:55 +0100

    server connected with bd

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import listEndpoints from "express-list-endpoints";
 import productsRoute from "./services/products/routes.js";
 import reviewsRoute from "./services/reviews/routes.js";
-// import createDefaultTables from "./db/create-tables.js";
+import { testConnection, connectDB } from "./db/index.js";
 
 const server = express();
 
@@ -16,7 +16,8 @@ server.use("/reviews", reviewsRoute);
 console.table(listEndpoints(server));
 server.listen(PORT, async () => {
   console.log(`✅ Server is running on port ${PORT}`);
-  //   await createDefaultTables();
+  await testConnection();
+  await connectDB();
 });
 
 server.on("error", console.log);
