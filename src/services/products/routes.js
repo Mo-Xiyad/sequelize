55f9d57d8fa4573e38910ba5fbaf2d9ae965b9a0
Zee <76219804+Mo-Xiyad@users.noreply.github.com>
2021-11-04 00:56:34 +0100

    rout

--- src/services/products/routes.js
+++ src/services/products/routes.js
@@ -4,6 +4,15 @@ import productHandler from "./handlers.js";
 
 const router = express.Router();
 
-router.route("/").post(productHandler.createproduct);
+router
+  .route("/")
+  .get(productHandler.getAllProducts)
+  .post(productHandler.createproduct);
+
+router
+  .route("/:id")
+  .get(productHandler.getProductById)
+  .put(productHandler.updateProduct)
+  .delete(productHandler.deleteProduct);
 
 export default router;
